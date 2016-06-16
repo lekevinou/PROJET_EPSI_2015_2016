aef7d7c7ec416aa171324de85afe412baa1b521c
Thomas <thomas.soub@gmail.com>
2016-06-17 00:41:09 +0200

    maj algo

--- algo_combat.php
+++ algo_combat.php
@@ -26,7 +26,7 @@ Class Unite{
 	}
 
 	public function getAttaque(){
-		return $this->_type;
+		return $this->_attaque;
 	}
 
 	public function setAttaque($attaque){
@@ -67,49 +67,86 @@ Class Unite{
 	}
 
 }
-/*
+
 Class Armee{
 
-	var $unites = array();
+	private $unites = array();
+
+	public function getUnites(){
+		return $this->_unites;
+	}
+
+	public function setUnites($unites){
+		$this->_unites = $unites;
+	}
+
+	public function __construct($unites){
+		$this->setUnites($unites);
+	}
 
 	public function calculAttaque(){
 
-		private $nbInfant = 0;
-		private $nbDistant = 0;
-		private $nbCavalier = 0;
-		private $nbTotal = 0;
+		$mesUnites = array();
+		$mesUnites = $this->getUnites();
 
-		foreach ($unite as $monUnite) {
-			if($monUnite.type == Infanterie)
-				$nbInfant++;
-			elseif($monUnite.type == Distant)
-				$nbDistant++;
-			elseif($monUnite.type == Cavalier)
-				$nbCavalier++;
+		$nbInfant = 0;
+		$nbDistant = 0;
+		$nbCavalier = 0;
+		$nbTotal = 0;
+		$attaqueTotale = 0;
+		$random = rand (0 , 10)/100;
+		echo $random;
+		foreach ($mesUnites as $monUnite) {
 
-			$nbTotal++;	
+			$type = $monUnite->getType();
+
+			switch($type){
+				case 'Infanterie':
+					$nbInfant++;
+					break;
+				case 'Distant':
+					$nbDistant++;
+					break;
+				case 'Cavalier':
+			 		$nbCavalier++;
+			 		break;
+
+			}
+
+			$nbTotal++;
+			$attaqueTotale = $attaqueTotale + $monUnite->getAttaque();
 		}
 
 		$resultatAttaque = array();
-		$resultatAttaque['ratioInfant'] => $nbInfant / $nbTotal;
-		$resultatAttaque['ratioDistant'] => $nbDistant / $nbTotal;
-		$resultatAttaque['ratioCavalier'] => $nbCavalier / $nbTotal;
-		$resultatAttaque['ratioTotal'] => $nbTotal;
+		$resultatAttaque['ratioInfant'] = $nbInfant / $nbTotal;
+		$resultatAttaque['ratioDistant'] = $nbDistant / $nbTotal;
+		$resultatAttaque['ratioCavalier'] = $nbCavalier / $nbTotal;
+		$resultatAttaque['Total'] = $nbTotal;
+		$resultatAttaque['attaqueTotale'] = $attaqueTotale + $attaqueTotale * $random;
+
+		print_r($resultatAttaque);
 
 		return $resultatAttaque;
 	}
 
-	public function calculDefense($resultatAttaque, $unites){
+	public function calculDefense($resultatAttaque){
 
-		var $defInfant = 0;
-		var $defDistant = 0;
-		var $defCavalier = 0;
-		var $defTotal = 0;
+		//print_r($resultatAttaque);
 
-		foreach ($unite as $monUnite) {
-			$defInfant => $defInfant + $monUnite.defInfant;
-			$defDistant => $defDistant + $monUnite.defDistant;
-			$defCavalier => $defCavalier + $monUnite.defCavalier;	
+
+		$mesUnites = array();
+		$mesUnites = $this->getUnites();
+
+		$defInfant = 0;
+		$defDistant = 0;
+		$defCavalier = 0;
+		$defTotal = 0;
+		$random = rand (0 , 10)/100;
+
+		foreach ($mesUnites as $monUnite) {
+			$defInfant = $defInfant + $monUnite->getDefInfant();
+			$defDistant = $defDistant + $monUnite->getDefDistant();
+			$defCavalier = $defCavalier + $monUnite->getDefCavalier();	
 		}
 
 		$defInfant = $defInfant * $resultatAttaque['ratioInfant'];
@@ -117,19 +154,57 @@ Class Armee{
 		$defCavalier = $defCavalier * $resultatAttaque['ratioCavalier'];
 
 		$defTotal = $defInfant + $defDistant + $defCavalier;
-
-		if($defTotal >= $resultatAttaque['ratioTotal'])
+		$defTotal = $defTotal + $defTotal * $random;
+		echo $defTotal;
+		if($defTotal >= $resultatAttaque['attaqueTotale']){
+			echo 'La defense gagne !';
 			return true;
-		else
+		}else{
+			echo 'L\'attaque gagne !';
 			return false;
+		 }
+		}
 
 }
-*/
 
-$barbare = new Unite("Barbare1", "Barbare", 20, 5, 20, 15);
-$archer = new Unite("Archer1", "Distant", 20, 5, 20, 15);
+$rodeur = new Unite("Rodeur", "Infanterie", 5, 5, 5, 5);
+$barbare = new Unite("Barbare", "Infanterie", 20, 10, 15, 5);
+$piquier = new Unite("Piquier", "Infanterie", 10, 20, 10, 40);
+$fantassin = new Unite("Fantassin", "Infanterie", 25, 20, 20, 10);
+$paladin = new Unite("Paladin", "Infanterie", 35, 25, 30, 15);
+$archer = new Unite("Archer", "Distant", 20, 5, 20, 15);
+$arbalerier = new Unite("Arbaletrier", "Distant", 30, 40, 20, 10);
+$cavalierLeger = new Unite("Cavalier Leger", "Cavalier", 30, 30, 10, 15);
+$cavalierLourd = new Unite("Cavalier Lourd", "Cavalier", 50, 35, 15, 20);
 
-echo $barbare->getNom();
-echo $archer->getNom();
+$tableau[] = $archer;
+$tableau[] = $barbare;
+$tableau[] = $piquier;
+$tableau[] = $fantassin;
+$tableau[] = $paladin;
+$tableau[] = $arbalerier;
+$tableau[] = $cavalierLeger;
+$tableau[] = $cavalierLourd;
+
+$tableau2[] = $cavalierLourd;
+$tableau2[] = $cavalierLourd;
+$tableau2[] = $cavalierLourd;
+$tableau2[] = $cavalierLourd;
+$tableau2[] = $cavalierLourd;
+$tableau2[] = $cavalierLourd;
+$tableau2[] = $cavalierLourd;
+$tableau2[] = $cavalierLourd;
+
+$armee = new Armee($tableau);
+$armee2 = new Armee($tableau2);
+
+//echo $barbare->getNom();
+//echo $archer->getNom();
+
+$resultat = array();
+$resultat = $armee->calculAttaque();
+
+$armee2->calculDefense($resultat);
+
 
 ?>
